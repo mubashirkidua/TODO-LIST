24cb5ba2462b9f84be3607658c758facbcaa8b9a
Muhammad Mubashir Ali <alimuhammad98573@gmail.com>
2024-04-08 03:12:04 -0700

    my first commit

---
+++ Index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What would you like to add in your todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Would you like to add more in your todos?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
